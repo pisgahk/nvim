a1936218abf5e72e5ca5c19471817650605e3374
pisgahk <kiyingipisgah@gmail.com>
2025-06-08 23:03:03 +0530

    Added git graph.

--- lua/plugins/configs.lua
+++ lua/plugins/configs.lua
@@ -1,9 +1,10 @@
 return {
-    require("plugins.configs.conform"),   -- Formatter
-    require("plugins.configs.lsp"),       -- LSP setup
-    require("plugins.configs.mason"),     -- Mason LSP & DAP servers
-    require("plugins.configs.nvim-cmp"),  -- Autocompletion
-    require("plugins.configs.treesitter"), -- Treesitter syntax highlighting
-    require("plugins.configs.lspconfig")  -- for the actual lsp setup.
-   -- require("plugins.configs.luasnip") -- for luasnip snippets.
+	require("plugins.configs.conform"), -- Formatter
+	require("plugins.configs.lsp"), -- LSP setup
+	require("plugins.configs.mason"), -- Mason LSP & DAP servers
+	require("plugins.configs.nvim-cmp"), -- Autocompletion
+	require("plugins.configs.treesitter"), -- Treesitter syntax highlighting
+	require("plugins.configs.lspconfig"), -- for the actual lsp setup.
+	-- require("plugins.configs.luasnip") -- for luasnip snippets.
+	--require("plugins.configs.gitgraph"), -- Adds the visual Graph for git.
 }
